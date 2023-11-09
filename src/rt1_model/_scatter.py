12e4a35334f11558ee367b0a65fb01c3a4922e66
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-11-09 14:35:02 +0100

    fix performance issues with sympys .is_constant()

--- src/rt1_model/_scatter.py
+++ src/rt1_model/_scatter.py
@@ -77,7 +77,10 @@ class _Scatter(object):
         # if _func is a constant, lambdify will create a function that returns a scalar
         # which is not suitable for further processing. in that case, vectorize the
         # obtained function
-        if self._func.is_constant():
+
+        # TODO maybe find a better check for this
+        #if self._func.is_constant():   # this is too slow!
+        if len(self._func.free_symbols) == 0:
             pfunc = np.vectorize(pfunc)
 
         return pfunc
