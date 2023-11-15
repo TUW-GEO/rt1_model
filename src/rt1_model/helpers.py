d716ee426baa55cbae88899250f41ef9c5b24b24
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-11-15 15:24:20 +0100

    upload helpers module

---
+++ src/rt1_model/helpers.py
@@ -0,0 +1,74 @@
+from numpydoc.docscrape import NumpyDocString
+
+
+def append_numpy_docstring(*funcs):
+    """
+    Decorator to combine numpy docstrings.
+
+    Parameter descriptions that already exist in the inherited docstrings will be
+    replaced by the description of the parameter in the function docstring!
+
+    Parameters
+    ----------
+    funcs :
+        Objects with __doc__ attribute defined.
+
+    Returns
+    -------
+    f:
+        The object with combined __doc__ sections.
+
+    """
+
+    def inner(decorated_f):
+        parent_docstring = decorated_f.__doc__
+        if parent_docstring is None:
+            parent_docstring = ""
+
+        comb = NumpyDocString(parent_docstring)
+
+        for f in funcs:
+            if not f.__doc__:
+                continue
+
+            f_doc = NumpyDocString(f.__doc__)
+
+            # combine lists of summaries with a newline
+            for key in ["Summary", "Extended Summary"]:
+                if f_doc[key]:
+                    comb[key] += ["\n", *f_doc[key]]
+
+            # combine references and example strings with a newline
+            for key in ["References", "Examples"]:
+                if f_doc[key]:
+                    comb[key] += "\n" + f_doc[key]
+
+            # combine parameter descriptions (override existing descriptions)
+            for p in f_doc["Parameters"]:
+                # check if parameter is already defined
+                old_p = any(i for i in comb["Parameters"] if i.name == p.name)
+                # if no description exists, append the parameter
+                if not old_p:
+                    comb["Parameters"] += [p]
+
+            for key in [
+                "Returns",
+                "Yields",
+                "Receives",
+                "Raises",
+                "Warns",
+                "Other Parameters",
+                "Attributes",
+                "Methods",
+                "See Also",
+                "Notes",
+                "Warnings",
+            ]:
+                comb[key] += f_doc[key]
+
+        # set the combined docstring
+        decorated_f.__doc__ = str(comb)
+
+        return decorated_f
+
+    return inner
