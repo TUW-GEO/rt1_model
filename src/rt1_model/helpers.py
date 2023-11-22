e047e1bdbc92314c5e90b374c61a286bd4983a83
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-11-22 16:21:20 +0100

    avoid depending on (full) numpydoc and allow errors during docstring parse

--- src/rt1_model/helpers.py
+++ src/rt1_model/helpers.py
@@ -1,4 +1,5 @@
-from numpydoc.docscrape import NumpyDocString
+from . import _log
+from ._numpydoc_docscrape import NumpyDocString
 
 
 def append_numpy_docstring(*funcs):
@@ -21,54 +22,59 @@ def append_numpy_docstring(*funcs):
     """
 
     def inner(decorated_f):
-        parent_docstring = decorated_f.__doc__
-        if parent_docstring is None:
-            parent_docstring = ""
+        try:
+            parent_docstring = decorated_f.__doc__
+            if parent_docstring is None:
+                parent_docstring = ""
 
-        comb = NumpyDocString(parent_docstring)
+            comb = NumpyDocString(parent_docstring)
 
-        for f in funcs:
-            if not f.__doc__:
-                continue
+            for f in funcs:
+                if not f.__doc__:
+                    continue
 
-            f_doc = NumpyDocString(f.__doc__)
+                f_doc = NumpyDocString(f.__doc__)
 
-            # combine lists of summaries with a newline
-            for key in ["Summary", "Extended Summary"]:
-                if f_doc[key]:
-                    comb[key] += ["\n", *f_doc[key]]
+                # combine lists of summaries with a newline
+                for key in ["Summary", "Extended Summary"]:
+                    if f_doc[key]:
+                        comb[key] += ["\n", *f_doc[key]]
 
-            # combine references and example strings with a newline
-            for key in ["References", "Examples"]:
-                if f_doc[key]:
-                    comb[key] += "\n" + f_doc[key]
+                # combine references and example strings with a newline
+                for key in ["References", "Examples"]:
+                    if f_doc[key]:
+                        comb[key] += "\n" + f_doc[key]
 
-            # combine parameter descriptions (override existing descriptions)
-            for p in f_doc["Parameters"]:
-                # check if parameter is already defined
-                old_p = any(i for i in comb["Parameters"] if i.name == p.name)
-                # if no description exists, append the parameter
-                if not old_p:
-                    comb["Parameters"] += [p]
+                # combine parameter descriptions (override existing descriptions)
+                for p in f_doc["Parameters"]:
+                    # check if parameter is already defined
+                    old_p = any(i for i in comb["Parameters"] if i.name == p.name)
+                    # if no description exists, append the parameter
+                    if not old_p:
+                        comb["Parameters"] += [p]
 
-            for key in [
-                "Returns",
-                "Yields",
-                "Receives",
-                "Raises",
-                "Warns",
-                "Other Parameters",
-                "Attributes",
-                "Methods",
-                "See Also",
-                "Notes",
-                "Warnings",
-            ]:
-                comb[key] += f_doc[key]
+                for key in [
+                    "Returns",
+                    "Yields",
+                    "Receives",
+                    "Raises",
+                    "Warns",
+                    "Other Parameters",
+                    "Attributes",
+                    "Methods",
+                    "See Also",
+                    "Notes",
+                    "Warnings",
+                ]:
+                    comb[key] += f_doc[key]
 
-        # set the combined docstring
-        decorated_f.__doc__ = str(comb)
-
+            # set the combined docstring
+            decorated_f.__doc__ = str(comb)
+        except Exception:
+            _log.debug(
+                "There was an error while trying to combine the docstring "
+                f"of {decorated_f} with the docstrings from {funcs}"
+            )
         return decorated_f
 
     return inner
